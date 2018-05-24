647d3392f012847e7affa13242a81c70f74e68e6
Rafael Bertolli <Rafael@yami.com.br>
2018-05-23 22:26:56 -0300

    Update use_trans_sid.php

--- public_dev/PhpSecInfo/Test/Session/use_trans_sid.php
+++ public_dev/PhpSecInfo/Test/Session/use_trans_sid.php
@@ -10,7 +10,7 @@
 /**
  * require the PhpSecInfo_Test_Session class
  */
-require_once('PhpSecInfo/Test/Test_Session.php');
+require_once(__DIR__.'/../../PhpSecInfo/Test/Test_Session.php');
 
 /**
  * Test class for session use_trans_sid
