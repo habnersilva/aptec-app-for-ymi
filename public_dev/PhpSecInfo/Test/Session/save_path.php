998c317f2962383527ab8869be49efce5fc76d88
Rafael Bertolli <Rafael@yami.com.br>
2018-05-23 22:26:43 -0300

    Update save_path.php

--- public_dev/PhpSecInfo/Test/Session/save_path.php
+++ public_dev/PhpSecInfo/Test/Session/save_path.php
@@ -9,7 +9,7 @@
 /**
  * require the PhpSecInfo_Test_Core class
  */
-require_once('PhpSecInfo/Test/Test_Session.php');
+require_once(__DIR__.'/../../PhpSecInfo/Test/Test_Session.php');
 
 /**
  * Test class for session save_path
