1eea1e53719b7a55e6316210818014423c89ae55
habnersilva <habner@aptec.com.br>
2018-06-12 16:31:51 -0300

    Remove echo teste

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
-echo 'teste';
-//require_once __DIR__ . "/../bootstrap/autoload.php";
+require_once __DIR__ . "/../bootstrap/autoload.php";
 
 // $start = new Aquarium\System();
 // $start->run();
